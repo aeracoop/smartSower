e69f6f1055c7fff6a721f86a4769e908d6888fe6
Lot <lotelx@users.noreply.github.com>
2020-09-26 22:21:42 +0200

    Update SmartSower.ino
    First candidate release 1.0

--- SmartSower.ino
+++ SmartSower.ino
@@ -20,17 +20,24 @@
 const int minRC = 1200; //value to trigger betwen Low & Med
 const int maxRC = 1800; //value to trigger betwen Med & High
 
-int rate1 = 35; //Rate 1 of seeds
-int rate2 = 30; //Rate 2 of seeds
+int rate1 = 45; //Rate 1 of seeds
+int rate2 = 50; //Rate 2 of seeds
+
+// 35 - > Old SmartSower 95 seeds/second
 
 Servo servoRate; // Object Servo from library
 
 unsigned long int ft, lt, x, tfail;
 int ch, chx[9][13];
 
+int state = -1;
+int nextState = 0;
 int rate = 0;
 int dist = 0;
 int zero = 0;
+boolean noise = false;
+int tNoise = 0;
+
 
 const int idx   = 10;
 const int total = 11;
@@ -50,23 +57,7 @@ void setup() {
   inizializeVectors();
   pinMode(2, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(2), decodePPM, FALLING);
-  servoRate.write(zero); //Close the door
-}
-
-void test3x3() {
-  setServo(rate1, 3000);
-  servoRate.write(zero);
-  delay(5000);
-
-  setServo(rate1, 3000);
-  servoRate.write(zero);
-  delay(5000);
-
-  setServo(rate1, 3000);
-  servoRate.write(zero);
-  delay(5000);
-  delay(500000);
-}
+  //servoRate.write(zero); //Close the door
 }
 
 void inizializeVectors() {
@@ -85,11 +76,11 @@ void setServo(int s, int time)
   do
   {
     servoRate.write(rate1 - 5);
-    delay(200);
+    //delay(100);
     servoRate.write(rate1);
-    delay(200);
+    //delay(200);
     servoRate.write(rate1 - 5);
-    delay(200);
+    //delay(100);
   } while ((millis() - t) < time);
 }
 
@@ -97,23 +88,49 @@ void loop() {
   if ((millis() - tfail) > 500) {
     Serial.println("Disconnect");
   } else {
+
     if (chx[6][val] < minRC)
-      rate = 0;
+      nextState = 0;
+    if (chx[6][val] > minRC && chx[6][val] < maxRC)
+      nextState = 1;
+    if (chx[6][val] > maxRC)
+      nextState = 2;
+
+  if (state != nextState){
+  state = nextState;
+  switch(state){
+    case 0: rate = zero; break;
+    case 1: rate = rate1;break;
+    case 2: rate = rate2;break;
+    default: rate = zero;
+  }
+  }
+    /*
+    if (chx[6][val] < minRC)
+      rate = zero;
     if (chx[6][val] > minRC && chx[6][val] < maxRC)
       rate = rate1;
     if (chx[6][val] > maxRC)
-      rate = rate2;
+      rate = rate2;*/
+
+    //setServo(rate,100);
+    servoNoise();    
     servoRate.write(rate);
 
-    Serial.print("CH7: "); Serial.print(chx[6][val]);
-    Serial.print("Rate: "); Serial.print(rate);   Serial.print("\t");
-
+    Serial.print(" CH7: "); Serial.print(chx[6][val]);
+    Serial.print(" Rate: "); Serial.print(rate);  
+    Serial.print(" State: "); Serial.print(state);   Serial.print("\t");
+    
+/* Code for Motor spreader
     if (chx[7][val] < minRC)
       dist = 0;
     if (chx[7][val] > minRC && chx[7][val] < maxRC)
       dist = dist1;
     if (chx[7][val] > maxRC)
       dist = dist2;
+
+      
+      */
 
     Serial.print("CH8: "); Serial.print(chx[7][val]);
     Serial.print("Dist: "); Serial.print(dist);
@@ -122,6 +139,30 @@ void loop() {
   }
 }
 
+void test3x3() {
+  setServo(rate1, 3000);
+  servoRate.write(zero);
+  delay(5000);
+
+  setServo(rate1, 3000);
+  servoRate.write(zero);
+  delay(5000);
+
+  setServo(rate1, 3000);
+  servoRate.write(zero);
+  delay(5000);
+  delay(500000);
+}
+void servoNoise(){
+      if (tNoise + 1000 > millis()){
+      tNoise = millis();
+      noise = !noise;
+      if (noise)
+        rate = rate+5;
+      else
+        rate = rate-5;
+    }
+}
 void decodePPM() {
   lt    = micros();
   tfail = millis();
@@ -144,11 +185,12 @@ void decodePPM() {
   }
 }
 
-
-/*Serial.print("CH1: "); Serial.print(chx[0][val]); Serial.print("\t");
+void printChannels(){
+  Serial.print("CH1: "); Serial.print(chx[0][val]); Serial.print("\t");
   Serial.print("CH2: "); Serial.print(chx[1][val]); Serial.print("\t");
   Serial.print("CH3: "); Serial.print(chx[2][val]); Serial.print("\t");
   Serial.print("CH4: "); Serial.print(chx[3][val]); Serial.print("\t");
   Serial.print("CH5: "); Serial.print(chx[4][val]); Serial.print("\t");
   Serial.print("CH6: "); Serial.print(chx[5][val]); Serial.print("\t");
-  Serial.print("CH9: ");Serial.println(chx[8][val]);*/
+  Serial.print("CH9: ");Serial.println(chx[8][val]);
+}
