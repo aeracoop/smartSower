1ba7d86b66cb5f545acdfbb93406252c6f4e9fee
Lot <lotelx@users.noreply.github.com>
2020-10-06 22:22:39 +0200

    Update SmartSower.ino
    This was the final version used in Almeria Sowing Operation Sept'2020

--- SmartSower.ino
+++ SmartSower.ino
@@ -20,8 +20,8 @@
 const int minRC = 1200; //value to trigger betwen Low & Med
 const int maxRC = 1800; //value to trigger betwen Med & High
 
-int rate1 = 45; //Rate 1 of seeds
-int rate2 = 50; //Rate 2 of seeds
+int rate1 = 36; //Rate 1 of seeds
+int rate2 = 39; //Rate 2 of seeds
 
 // 35 - > Old SmartSower 95 seeds/second
 
@@ -50,14 +50,18 @@ void setup() {
   servoRate.attach(9);  // attach servo to digital pin 9
 
   //If servo works counterwise:
+  
   rate1 = 180 - rate1;
   rate2 = 180 - rate2;
   zero = 180;
 
+
+
   inizializeVectors();
   pinMode(2, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(2), decodePPM, FALLING);
-  //servoRate.write(zero); //Close the door
+  servoRate.write(zero); //Close the door
+  //calibration5();
 }
 
 void inizializeVectors() {
@@ -69,20 +73,6 @@ void inizializeVectors() {
   }
 }
 
-//This function adds artifical noise in order the facilitate the fall of the seeds.
-void setServo(int s, int time)
-{
-  int t = millis();
-  do
-  {
-    servoRate.write(rate1 - 5);
-    //delay(100);
-    servoRate.write(rate1);
-    //delay(200);
-    servoRate.write(rate1 - 5);
-    //delay(100);
-  } while ((millis() - t) < time);
-}
 
 void loop() {
   if ((millis() - tfail) > 500) {
@@ -96,50 +86,74 @@ void loop() {
     if (chx[6][val] > maxRC)
       nextState = 2;
 
-  if (state != nextState){
-  state = nextState;
-  switch(state){
-    case 0: rate = zero; break;
-    case 1: rate = rate1;break;
-    case 2: rate = rate2;break;
-    default: rate = zero;
-  }
-  }
-    /*
-    if (chx[6][val] < minRC)
-      rate = zero;
-    if (chx[6][val] > minRC && chx[6][val] < maxRC)
-      rate = rate1;
-    if (chx[6][val] > maxRC)
-      rate = rate2;*/
-
-    //setServo(rate,100);
-    servoNoise();    
+    if (state != nextState) {
+      state = nextState;
+      switch (state) {
+        case 0: rate = zero; break;
+        case 1: rate = rate1; break;
+        case 2: rate = rate2; break;
+        default: rate = zero;
+      }
+    }
+    servoNoise();
     servoRate.write(rate);
 
     Serial.print(" CH7: "); Serial.print(chx[6][val]);
-    Serial.print(" Rate: "); Serial.print(rate);  
+    Serial.print(" Rate: "); Serial.print(rate);
     Serial.print(" State: "); Serial.print(state);   Serial.print("\t");
-    
-/* Code for Motor spreader
-    if (chx[7][val] < minRC)
-      dist = 0;
-    if (chx[7][val] > minRC && chx[7][val] < maxRC)
-      dist = dist1;
-    if (chx[7][val] > maxRC)
-      dist = dist2;
 
-      
-      */
+    /* Code for Motor spreader
+        if (chx[7][val] < minRC)
+          dist = 0;
+        if (chx[7][val] > minRC && chx[7][val] < maxRC)
+          dist = dist1;
+        if (chx[7][val] > maxRC)
+          dist = dist2;
 
-    Serial.print("CH8: "); Serial.print(chx[7][val]);
-    Serial.print("Dist: "); Serial.print(dist);
-    Serial.print("\t");
+        Serial.print("CH8: "); Serial.print(chx[7][val]);
+        Serial.print("Dist: "); Serial.print(dist);
+        Serial.print("\t");*/
+
     Serial.println();
   }
 }
 
-void test3x3() {
+void calibration5() {
+  int calibRate = 0;
+  servoRate.write(zero);
+
+
+  calibRate = 180 - 36;
+  Serial.println("Open 40");
+  setServo(calibRate, 1000);
+  servoRate.write(zero);
+  Serial.println("Close");
+  delay(5000);
+
+  calibRate = 180 - 37;
+  Serial.println("Open 40");
+  setServo(calibRate, 1000);
+  servoRate.write(zero);
+  Serial.println("Close");
+  delay(5000);
+
+  calibRate = 180 - 38;
+  Serial.println("Open 40");
+  setServo(calibRate, 1000);
+  servoRate.write(zero);
+  Serial.println("Close");
+  delay(5000);
+
+  calibRate = 180 - 39;
+  Serial.println("Open 40");
+  setServo(calibRate, 1000);
+  servoRate.write(zero);
+  Serial.println("Close");
+  delay(5000);
+}
+
+/*
+  void test3x3() {
   setServo(rate1, 3000);
   servoRate.write(zero);
   delay(5000);
@@ -152,17 +166,32 @@ void test3x3() {
   servoRate.write(zero);
   delay(5000);
   delay(500000);
+  }*/
+
+//This function adds artifical noise in order the facilitate the fall of the seeds.
+void servoNoise() {
+  if (tNoise + 1000 > millis()) {
+    tNoise = millis();
+    noise = !noise;
+    if (noise)
+      rate = rate + 5;
+    else
+      rate = rate - 5;
+  }
 }
-void servoNoise(){
-      if (tNoise + 1000 > millis()){
-      tNoise = millis();
-      noise = !noise;
-      if (noise)
-        rate = rate+5;
-      else
-        rate = rate-5;
-    }
+
+void setServo(int r, int time)
+{
+  int t = millis();
+  do
+  {
+    servoRate.write(r - 3);
+    delay(200);
+    servoRate.write(r);
+    delay(200);
+  } while ((millis() - t) <= time);
 }
+
 void decodePPM() {
   lt    = micros();
   tfail = millis();
@@ -185,12 +214,12 @@ void decodePPM() {
   }
 }
 
-void printChannels(){
+void printChannels() {
   Serial.print("CH1: "); Serial.print(chx[0][val]); Serial.print("\t");
   Serial.print("CH2: "); Serial.print(chx[1][val]); Serial.print("\t");
   Serial.print("CH3: "); Serial.print(chx[2][val]); Serial.print("\t");
   Serial.print("CH4: "); Serial.print(chx[3][val]); Serial.print("\t");
   Serial.print("CH5: "); Serial.print(chx[4][val]); Serial.print("\t");
   Serial.print("CH6: "); Serial.print(chx[5][val]); Serial.print("\t");
-  Serial.print("CH9: ");Serial.println(chx[8][val]);
+  Serial.print("CH9: "); Serial.println(chx[8][val]);
 }
